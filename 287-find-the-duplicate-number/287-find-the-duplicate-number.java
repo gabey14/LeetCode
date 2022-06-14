563175905ab4a8a048450502060ee424667efc88
Abey George <69460299+gabey14@users.noreply.github.com>
2022-06-14 16:27:42 +0530

    Time: 7 ms (57.98%), Space: 76.4 MB (24.68%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,23 +1,22 @@
 class Solution {
     public int findDuplicate(int[] nums) {
 //         Cyclic Sort the array
-      int i = 0;
-        while(i < nums.length){
-            int correct = nums[i] - 1;  // c 
-            if(nums[i] != nums[correct]){
-                int temp = nums[correct];
-                nums[correct] = nums[i];
-                nums[i] = temp;
-            }else{
+       int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != i + 1) {
+                int correct = nums[i] - 1;
+                if (nums[i] != nums[correct]) {
+                    int temp = nums[correct];
+                    nums[correct] = nums[i];
+                    nums[i] = temp;
+                } else {
+                    return nums[i];
+                }
+            } else {
                 i++;
             }
+
         }
-        
-        for(int index=0;index < nums.length;index++){
-            if(nums[index] != index + 1){
-                return nums[index];
-            }
-        }
-        return nums.length;
+        return -1;
     }
 }
